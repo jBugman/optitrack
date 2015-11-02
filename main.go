a3119975a263f734b72b1983d95b0ca5a4a3ede2
Sergey Parshukov <sergey@interactivelab.ru>
2015-11-02 21:03:53 +0300

    Refactored recording

--- main.go
+++ main.go
@@ -1,88 +1,101 @@
 package main
 
 import (
-	"log"
-	"net"
-	//	"fmt"
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"math"
+	"net"
 	"os"
+	"time"
 )
 
-func main() {
-	addr, err := net.ResolveUDPAddr("udp4", "239.255.42.99:1511")
+const multicastAddress = "239.255.42.99:1511"
+
+func record() error {
+	log.Println("Recording packets")
+	addr, err := net.ResolveUDPAddr("udp4", multicastAddress)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	conn, err := net.ListenMulticastUDP("udp4", nil, addr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer conn.Close()
-
-	outfile, err := os.Create("outfile.bin")
+	filename := time.Now().Format("2005-01-02_15-04.bin")
+	outfile, err := os.Create(filename)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer outfile.Close()
 	defer outfile.Sync()
 
+	recieved := 0
 	buf := make([]byte, 5000)
-	for {
-		_, _, err := conn.ReadFromUDP(buf)
+	for i := 0; true; i++ {
+		n, _, err := conn.ReadFromUDP(buf)
 		if err != nil {
-			log.Println(err)
+			return err
 		}
-		fmt.Println(0)
-		//fmt.Println(buf)
 		outfile.Write(buf)
-
-		var offset int
-		iMessage, _ := binary.Uvarint(buf[offset : offset+2])
-		offset += 2
-		//numBytes, _ := binary.Uvarint(buf[offset:offset+2])
-		offset += 2
-		if iMessage != 7 { // 7 - mocap data
-			continue
-		}
-		//frame, _ := binary.Uvarint(buf[offset:offset+4])
-		offset += 4
-		numDataSets, _ := binary.Uvarint(buf[offset : offset+4])
-		offset += 4
-		//fmt.Println(iMessage, numBytes, frame, numDataSets)
-		var numMarkers int
-		var markers []byte
-		for ms := 0; ms < int(numDataSets); ms++ {
-			bb := new(bytes.Buffer)
-			var i int
-			for i = 0; buf[offset+i] != 0; i++ {
-				bb.WriteByte(buf[offset+i])
-			}
-			offset += i + 1
-			//fmt.Println(bb.String())
-			nm, _ := binary.Uvarint(buf[offset : offset+4])
-			numMarkers = int(nm)
-			offset += 4
-			nbytes := numMarkers * 3 * 4
-			markers = buf[offset : offset+nbytes]
-			offset += nbytes
-			//fmt.Println(numMarkers, nbytes, markers, buf[offset:])
-			for i = 0; i < numMarkers; i++ {
-				var x, y, z float64
-				off := 0
-				x0, _ := binary.Uvarint(markers[off : off+4])
-				x = math.Float64frombits(x0)
-				off += 4
-				y0, _ := binary.Uvarint(markers[off : off+4])
-				y = math.Float64frombits(y0)
-				off += 4
-				z0, _ := binary.Uvarint(markers[off : off+4])
-				z = math.Float64frombits(z0)
-				//fmt.Println(x, y, z)
-				_, _, _ = x, y, z
-			}
+		recieved += n
+		if i%100 == 0 {
+			log.Printf("Recieved %d bytes\n", recieved)
 		}
 	}
+	return nil
+}
+
+// Packet parsing attempt (not finished)
+func parsePacket(buf []byte) {
+	var offset int
+	iMessage, _ := binary.Uvarint(buf[offset : offset+2])
+	offset += 2
+	numBytes, _ := binary.Uvarint(buf[offset : offset+2])
+	offset += 2
+	if iMessage != 7 { // 7 - mocap data
+		return
+	}
+	frame, _ := binary.Uvarint(buf[offset : offset+4])
+	offset += 4
+	numDataSets, _ := binary.Uvarint(buf[offset : offset+4])
+	offset += 4
+	fmt.Println(iMessage, numBytes, frame, numDataSets)
+	var numMarkers int
+	var markers []byte
+	for ms := 0; ms < int(numDataSets); ms++ {
+		bb := new(bytes.Buffer)
+		var i int
+		for i = 0; buf[offset+i] != 0; i++ {
+			bb.WriteByte(buf[offset+i])
+		}
+		offset += i + 1
+		fmt.Println(bb.String())
+		nm, _ := binary.Uvarint(buf[offset : offset+4])
+		numMarkers = int(nm)
+		offset += 4
+		nbytes := numMarkers * 3 * 4
+		markers = buf[offset : offset+nbytes]
+		offset += nbytes
+		fmt.Println(numMarkers, nbytes, markers, buf[offset:])
+		for i = 0; i < numMarkers; i++ {
+			var x, y, z float64
+			off := 0
+			x0, _ := binary.Uvarint(markers[off : off+4])
+			x = math.Float64frombits(x0)
+			off += 4
+			y0, _ := binary.Uvarint(markers[off : off+4])
+			y = math.Float64frombits(y0)
+			off += 4
+			z0, _ := binary.Uvarint(markers[off : off+4])
+			z = math.Float64frombits(z0)
+			fmt.Println(x, y, z)
+		}
+	}
+}
+
+func main() {
+	record()
 }
